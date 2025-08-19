c0774f783be46262802da9a7f46d0c04346c3ef1
Darkness <mr.deni_2022@mail.ru>
2025-08-19 11:53:16 +0300

    Creating MVC project
    Creating MVC project used SPRING MVC + hibernate. add next classes from file config + controller + model + service. Don't use SessioFactory only EntityManager Get/Post mapping + used Anotation ReqestParam

---
+++ src/main/java/org/example/service/UserServiceImp.java
@@ -0,0 +1,43 @@
+package org.example.service;
+
+import org.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+
+public class UserServiceImp {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserServiceImp(UserService userService) {
+        this.userService = userService;
+    }
+
+    public List<User> findAll() {
+        return (List<User>) userService.findAll();
+    }
+
+    @Transactional
+    public void save(User user) {
+        userService.save(user);
+    }
+
+    @Transactional
+    public void delete(User user) {
+        userService.delete(user);
+    }
+
+    @Transactional
+    public void updateUser(User user) {
+        userService.save(user);
+    }
+
+    public User findById(Long id) {
+        return userService.findById(id).orElse(null);
+    }
+}
