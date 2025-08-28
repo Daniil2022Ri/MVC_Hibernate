181e96f857512a8244661cec1916ba87b22de3a5
Darkness <mr.deni_2022@mail.ru>
2025-08-28 20:27:17 +0300

    Update

--- src/main/java/org/example/config/AppInit.java
+++ src/main/java/org/example/config/AppInit.java
@@ -3,7 +3,6 @@ package org.example.config;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,24 +10,20 @@ import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{ AppConfig.class };
     }
-
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{ WebConfig.class };
     }
 
-
     @Override
     protected String[] getServletMappings() {
         return new String[]{ "/" };
     }
-
 
     @Override
     public void onStartup(ServletContext aServletContext) throws ServletException {
@@ -39,11 +34,8 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         encodingFilter.setInitParameter("encoding", "UTF-8");
         encodingFilter.setInitParameter("forceEncoding", "true");
         encodingFilter.addMappingForUrlPatterns(null, true, "/*");
-
-
         registerHiddenFieldFilter(aServletContext);
     }
-
 
     private void registerHiddenFieldFilter(ServletContext aContext) {
         aContext.addFilter("hiddenHttpMethodFilter",
--- src/main/java/org/example/service/UserServiceImp.java
+++ src/main/java/org/example/service/UserServiceImp.java
@@ -3,7 +3,6 @@ package org.example.service;
 import org.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
